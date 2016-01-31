44827612fd2d1a01b1de8bea7a050bec202c24e8
skiwi <frank_skiwi@hotmail.com>
2016-01-31 17:03:14 +0100

    Reduced memory usage by using some sort of Iterative Deepening Depth First Search.

--- src/main/java/com/skiwi/ogameplanner/Planner.java
+++ src/main/java/com/skiwi/ogameplanner/Planner.java
@@ -1,7 +1,6 @@
 package com.skiwi.ogameplanner;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
 import java.util.function.Predicate;
 
 /**
@@ -17,38 +16,113 @@ public class Planner {
     }
 
     public PlayerSnapshot plan() {
-        PlayerSnapshot earliestMatchingPlayerSnapshot = null;
+//        PlayerSnapshot earliestMatchingPlayerSnapshot = null;
 
         if (successPredicate.test(initialPlayerSnapshot)) {
             return initialPlayerSnapshot;
         }
 
-        PriorityQueue<PlayerSnapshot> queue = new PriorityQueue<>(Comparator.comparingLong(PlayerSnapshot::getTime));
-        queue.add(initialPlayerSnapshot);
-        while (!queue.isEmpty()) {
-            PlayerSnapshot playerSnapshot = queue.poll();
-            if (earliestMatchingPlayerSnapshot != null && playerSnapshot.getTime() >= earliestMatchingPlayerSnapshot.getTime()) {
-                continue;
-            }
+//        PriorityQueue<PlayerSnapshot> queue = new PriorityQueue<>(Comparator.comparingLong(PlayerSnapshot::getTime));
+//        queue.add(initialPlayerSnapshot);
+//        while (!queue.isEmpty()) {
+//            PlayerSnapshot playerSnapshot = queue.poll();
+//            if (earliestMatchingPlayerSnapshot != null && playerSnapshot.getTime() >= earliestMatchingPlayerSnapshot.getTime()) {
+//                continue;
+//            }
+//
+//            for (Action action : playerSnapshot.generateActions()) {
+//                if (action.isAllowed(playerSnapshot)) {
+//                    PlayerSnapshot resultingPlayerSnapshot = action.performAction(playerSnapshot);
+//                    if (successPredicate.test(resultingPlayerSnapshot)) {
+//                        if (earliestMatchingPlayerSnapshot == null) {
+//                            earliestMatchingPlayerSnapshot = resultingPlayerSnapshot;
+//                        } else {
+//                            if (resultingPlayerSnapshot.getTime() < earliestMatchingPlayerSnapshot.getTime()) {
+//                                earliestMatchingPlayerSnapshot = resultingPlayerSnapshot;
+//                            }
+//                        }
+//                    } else {
+//                        queue.add(resultingPlayerSnapshot);
+//                    }
+//                }
+//            }
+//
+//        return earliestMatchingPlayerSnapshot;
 
-            for (Action action : playerSnapshot.generateActions()) {
-                if (action.isAllowed(playerSnapshot)) {
-                    PlayerSnapshot resultingPlayerSnapshot = action.performAction(playerSnapshot);
-                    if (successPredicate.test(resultingPlayerSnapshot)) {
-                        if (earliestMatchingPlayerSnapshot == null) {
-                            earliestMatchingPlayerSnapshot = resultingPlayerSnapshot;
-                        } else {
-                            if (resultingPlayerSnapshot.getTime() < earliestMatchingPlayerSnapshot.getTime()) {
-                                earliestMatchingPlayerSnapshot = resultingPlayerSnapshot;
-                            }
+        long limit = 50;
+        while (true) {
+            limit *= 2;
+            System.out.println("limit = " + limit);
+//            PlayerSnapshot earliestMatchingPlayerSnapshot = dfsPlan(initialPlayerSnapshot, limit);
+//            if (earliestMatchingPlayerSnapshot != null) {
+//                return earliestMatchingPlayerSnapshot;
+//            }
+
+            PlayerSnapshot earliestMatchingSnapshot = null;
+
+            Deque<PlayerSnapshot> stack = new ArrayDeque<>();
+            stack.add(initialPlayerSnapshot);
+            while (!stack.isEmpty()) {
+                PlayerSnapshot playerSnapshot = stack.pop();
+
+                if (earliestMatchingSnapshot != null && playerSnapshot.getTime() >= earliestMatchingSnapshot.getTime()) {
+                    continue;
+                }
+
+                if (successPredicate.test(playerSnapshot)) {
+                    System.out.println("MATCH @ t = " + playerSnapshot.getTime());
+                    if (earliestMatchingSnapshot == null) {
+                        earliestMatchingSnapshot = playerSnapshot;
+                    }
+                    else {
+                        if (playerSnapshot.getTime() < earliestMatchingSnapshot.getTime()) {
+                            earliestMatchingSnapshot = playerSnapshot;
                         }
-                    } else {
-                        queue.add(resultingPlayerSnapshot);
+                    }
+                    continue;
+                }
+
+                if (playerSnapshot.getTime() > limit) {
+                    continue;
+                }
+
+                for (Action action : playerSnapshot.generateActions()) {
+                    if (action.isAllowed(playerSnapshot)) {
+                        //TODO maybe we can sort on actions that take the least time?
+                        stack.push(action.performAction(playerSnapshot));
                     }
                 }
             }
-        }
 
-        return earliestMatchingPlayerSnapshot;
+            if (earliestMatchingSnapshot != null) {
+                return earliestMatchingSnapshot;
+            }
+        }
     }
+
+//    private PlayerSnapshot dfsPlan(PlayerSnapshot playerSnapshot, long limit) {
+//        if (successPredicate.test(playerSnapshot)) {
+//            return playerSnapshot;
+//        }
+//        if (playerSnapshot.getTime() > limit) {
+//            return null;
+//        }
+//
+//        PlayerSnapshot earliestMatchingPlayerSnapshot = null;
+//        for (Action action : playerSnapshot.generateActions()) {
+//            PlayerSnapshot resultingPlayerSnapshot = dfsPlan(action.performAction(playerSnapshot), limit);
+//            if (resultingPlayerSnapshot != null && successPredicate.test(resultingPlayerSnapshot)) {
+//                if (earliestMatchingPlayerSnapshot == null) {
+//                    earliestMatchingPlayerSnapshot = resultingPlayerSnapshot;
+//                }
+//                else {
+//                    if (resultingPlayerSnapshot.getTime() < earliestMatchingPlayerSnapshot.getTime()) {
+//                        earliestMatchingPlayerSnapshot = resultingPlayerSnapshot;
+//                    }
+//                }
+//            }
+//        }
+//
+//        return earliestMatchingPlayerSnapshot;
+//    }
 }
